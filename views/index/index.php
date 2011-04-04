50e994b5fd2010661962fb0ad9b241e9b528bb01
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-04-04 20:19:53 +0000

    Added TDDD27 project information for deadline 4/4
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4103 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- views/index/index.php
+++ views/index/index.php
@@ -16,126 +16,58 @@
     </style>
 <?php $this->layout->exitSection(); ?>
 <div id="wrap">
-    <img alt="NanoMVC Logo" src="<?php echo url("/static/logo-nanomvc.png"); ?>" />
-    <h1>Welcome to NanoMVC <?php echo internal\VERSION; ?>!</h1>
-    <p>
-        You have successfully installed a...
-    </p>
+    <h1>TDDD27 - Advanced Web Programming</h1>
+    <p>Gruppen består av Per Jonsson (perjo984@student.liu.se). Projektadressen är http://eventapp.omnicloud.org.</p>
+
+    <h2>Projektbeskrivning</h2>
     <ul>
         <li>
-            ...compact and highly advanced database abstraction layer with a
-            modeling system that allows native php instance access to stored
-            instances of data.
+        Webbapplikation för att hantera en enkel medlemsdatabas samt inbjudningar till evenemang.
+        </l>
+        <li>
+        Applikationen ska nyttjas av ett nätverk där olika städer runt om i världen fungerar som hubbar där evenemang arrangeras.
         </li>
         <li>
-            ...lightweight and highly useful view renderer which use dynamic
-            layouting and supports nested sections of content.
+        Fyra typer av användare ska finnas i applikationen: gäster, medlemmar, arrangörer och administratörer. Varje användare har en särskild behörighetsnivå som låter denne utföra vissa bestämda aktiviteter i applikationen.
+
         </li>
         <li>
-            ...controller architecture that allows direct access of data and
-            functions in the view without explicitly passing anything.
+När arrangörer gör en inbjudan till ett evenemang väljer de bland medlemmar i en lista.
+
+ skickas ett e-postmeddelande till de användare som arrangören väljer att bjuda in. Dessa användare har sedan möjlighet att besvara inbjudan genom att bekräfta eller avslå närvaro.
+
         </li>
         <li>
-            ...framework for content that uses native PHP for performance and
-            lower learning curve.
-        </li>
-        <li>
-            ...rich api that provides pragmatic generic solutions to everyday
-            programming problems such as listing files in directories and
-            piecing together and comparing arrays.
-        </li>
-        <li>
-            ...session handler with uses the database for storing session data
-            allowing multiple server instances to share the same session
-            information.
-        </li>
-        <li>
-            ...mailer with a built in PHP mail spooler that can handle temporary
-            SMTP outages.
-        </li>
-        <li>
-            ...mailer that handles any combination of plain text, html and
-            attachments making mail while being fully RFC compatible.
-        </li>
-        <li>
-            ...messenger which makes basic user notification as simple as it
-            should be.
-        </li>
-        <li>
-            ...interface generator called quantum model interface that enables
-            programming and refactoring of user interaction to be faster and
-            use less code duplication.
-        </li>
-        <li>
-            ...class autoloader that maps classes to file locations
-            unambigiously.
-        </li>
-        <li>
-            ...framework using code styles and conventions that make sense.
-        </li>
-        <li>
-            ...error handler and debugging api with informative error messages
-            that allows you to know what's wrong at a glance.
-        </li>
-        <li>
-            ...object oriented and non excessive class architecture
-            with focus on extendability.
-        </li>
-        <li>
-            ...framework which pragmatically utilizes namespaces which
-            prevents conflicts between nanomvc and external libraries
-            without making code more confusing and harder to read.
-        </li>
-        <li>
-            ...framework which smootly integrates with vendors and 3rd party code
-            and has built in PEAR autoload support.
-        </li>
-        <li>
-            ...type handler which allows you to juggle types like you're used
-            to with as little code as possible.
-        </li>
-        <li>
-            ...database selection abstractor which escapes your SQL and builds
-            queries for you.
-        </li>
-        <li>
-            ...graph relation architecture which allows modeling and selection
-            of complex enteprise level relational data structures a breeze.
-        </li>
-        <li>
-            ...built in file cacher which allows safe uploading of files and
-            images while letting the web server do the heavy static file deilvery
-            work.
+        Inloggning i systemet sker via Facebook-Connect med vanlig login som fallback.
         </li>
 
         <li>
-            ...thread library that allows two kinds of request forking in special
-            cases where heavy work is required without interrupting the
-            current request.
-        </li>
-        <li>
-            ...parallel request/thread syncronizer which utilizes automatic InnoDB
-            row locking to prevent data corruption when multiple requests access
-            the same data.
-        </li>
-        <li>
-            ...built in data structure syncronizer which allows you to develop
-            and refactor your models without even writing a line of SQL to keep
-            the data in sync with the database.
-        </li>
-        <li>
-            ...localization engine which automatically parses and exports/imports
-            the human text in your application to the popular PO gettext format.
-        </li>
-        <li>
-            ...collection of other useful developer tools that allows browser
-            interaction with the translation, deleting unused columns and reparing
-            broken mysql relations.
-        </li>
-    </ul>
+Ett publikt API kommer att skrivas så att data kan extraheras och presenteras på en extern Wordpress-baserad webbplats. Den data som ska presenteras består av evenemang i en viss hubb.
 
-    <p>
-        ...and much more! Now the fun begins..
-        &lt;Insert your web application here.&gt; ;)
-    </p>
+        </li>
+         <h2>Teknologier</h2>
+
+
+
+        <li>
+   Applikationen ska implementeras huvudsakligen i språket PHP.
+        </li>
+        <li>
+       För servern kommer ramverket NanoMVC att nyttjas.
+        </li>
+        <li>
+       För klienten kommer ramverket JQuery att nyttjas.
+        </li>
+        <li>
+       Databas kommer att vara MySQL.
+        </li>
+        <li>
+       Facebook API (Facebook Connect) kommer att användas för inloggningsfunktionen.
+        </li>
+        <li>
+       Subversion kommer att användas för versionshantering av projektet.
+        </li>
+
+         <h2>Fokus på betyg</h2>
+         <li>5</li>
 </div>

1c02b9b8d9ea6a3b7961bb25fca300a3a1114dab
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-04-04 19:54:22 +0000

    Added database to config
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4102 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- config.php
+++ config.php
@@ -15,3 +15,64 @@ namespace nmvc\internal {
         // Add modules that your application uses here.
     );
 }
+
+namespace nmvc\core\config {
+    const PHP_BINARY = 'php';
+    const SESSION_ENFORCE_HTTPS = false;
+    const SESSION_DOMAIN = NULL;
+    const DEFAULT_LANGUAGE = 'en';
+    const RECAPTCHA_PUBLIC_KEY = '';
+    const RECAPTCHA_PRIVATE_KEY = '';
+    const DISPLAY_DEVMODE_NOTICE = true;
+    const DOWN_MESSAGE = '';
+    const IGNORE_64_BIT_WARNING = false;
+    const TRANSLATION_MIN_Q = 0.4;
+    const TRANSLATION_ENABLED = false;
+    const PEAR_AUTOLOAD = false;
+    const ERROR_LOG = NULL;
+    const FORCE_ERROR_FLAGS = false;
+    const FORCE_ERROR_DISPLAY = false;
+    const MAINTENANCE_MODE = true;
+    const DEVELOPER_KEY = '';
+}
+
+
+namespace nmvc\db\config {
+    const STORAGE_ENGINE = 'auto';
+    const REQUEST_LEVEL_TRANSACTIONALITY = true;
+    const DEBUG_QUERY_BENCHMARK = false;
+    const USE_TRIGGER_SEQUENCING = true;
+    const PREFIX = '';
+    const NAME = 'u10006_event';
+    const PORT = 5000;
+    const PASSWORD = '5a4e40911eae0559';
+    const USER = 'u10006_event';
+    const HOST = 'webbhotell.omnicloud.org';
+}
+
+
+namespace nmvc\mail\config {
+    const SPOOL_RETRY_INTERVAL_SECONDS = 300;
+    const SMTP_AUTH_PASSWORD = 'password';
+    const SMTP_AUTH_USER = 'user';
+    const SMTP_AUTH_ENABLE = false;
+    const SMTP_TLS_ENABLE = false;
+    const SMTP_FROM_HOST = NULL;
+    const SMTP_TIMEOUT = 10;
+    const SMTP_PORT = 25;
+    const SMTP_HOST = 'localhost';
+    const FROM_ADDRESS = '';
+    const FROM_NAME = '';
+}
+
+
+namespace nmvc\userx\config {
+    const LAST_DENY_AUTOREDIRECT = true;
+    const SHELL_LOGIN = false;
+    const MULTIPLE_GROUPS = false;
+    const MULTIPLE_IDENTITIES = false;
+    const HASHING_ALGORITHM = 'crypt';
+    const SOFT_403 = false;
+    const REMEMBER_ME_DAYS = 356;
+    const SESSION_TIMEOUT_MINUTES = false;
+}

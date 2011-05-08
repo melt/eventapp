32e79250544043961dd21871aa53befe0dd62441
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-05-08 22:51:26 +0000

    Changed pdf to reports due to bad formatting
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4322 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,4 +1,4 @@
 <?php namespace nmvc; ?>
 <h3><a href="<?php echo url("/admin/spec"); ?>">High Level Specification</a></h3>
 <h3><a href="<?php echo url("/admin/login"); ?>">Login to App</a></h3>
-<h3><a href="<?php echo url("/static/pdf/20110507.pdf"); ?>">Status Check 2011-05-07</a></h3>
+<h3><a href="<?php echo url("/static/reports/20110507.html"); ?>">Status Check 2011-05-07</a></h3>

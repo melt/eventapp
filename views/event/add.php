1bc3f42a90b6b734b2529cc5c637bff43e5eacfa
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-06-06 21:28:34 +0000

    Added event for adding form
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4649 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- views/event/add.php
+++ views/event/add.php
@@ -1,22 +1,35 @@
 <?php namespace nmvc; ?>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var default_text = "<Add a teaser (one sentence of the overall topic of the evening, for example “April’s Fools”, “Summer dinner”; exciting special guests; etc.)>";
 
-
-<Add a teaser (one sentence of the overall topic of the evening, for example “April’s
-Fools”, “Summer dinner”; exciting special guests; etc.)>
-
-Please confirm your participation as soon as possible. You will receive a confirmation
-email with the name of the location a couple of days beforehand.
-
-
+        /*
+         * Shows a default text for the description textarea that is removed on focus and not submitted with form if left intact
+         **/
+        $(".fc_description textarea").val( default_text );
+        $(".fc_description textarea").focus(function() {
+            $(this).val("");
+        });
+        $(".fc_description textarea").blur(function() {
+            if($(this).val()=="")
+                $(this).val( default_text );
+        });
+        $("#new_event_form").submit(function() {
+            if($(".fc_description textarea").val()==default_text)
+                $(".fc_description textarea").val("");
+        });
+        
+  });
+</script>
 
 
 <div id="stylized" class="myform">
 <h1><?php echo _("Add new event"); ?></h1>
 <p><?php echo _("Use this form to add a new event to %s.",APP_NAME); ?></p>
 <?php $interface = new qmi\ModelInterface("new_event", "cell"); ?>
-<?php echo $interface->startForm(); ?>
+<?php echo $interface->startForm(array("id"=>"new_event_form")); ?>
 <?php echo $interface->getInterface($this->new_event); ?>
-<button href="<?php echo qmi\get_action_link($this->new_event, "addEventAndContinue"); ?>" role="button" aria-disabled="false"><?php echo _("Next Step"); ?></button> or <a href="<?php echo url("/"); ?>">Cancel</a>
+<button type="submit "role="button" aria-disabled="false"><?php echo _("Next Step"); ?></button> or <a href="<?php echo url("/"); ?>">Cancel</a>
 <div class="spacer"></div>
 <?php echo $interface->finalizeForm(false); ?>
 </div>

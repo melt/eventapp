694e9a82bd46c4666ad7921a06d62a486bdeb712
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-06-07 23:01:09 +0000

    Updated rvsp
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4697 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- views/outside/rvsp_thanks.php
+++ views/outside/rvsp_thanks.php
@@ -1,9 +1,9 @@
 <?php namespace nmvc; ?>
 <div id="stylized" class="myform">
     <h1><?php echo _("Thanks for responding!"); ?></h1>
-    <?php if($this->rvsp==1): ?>
-    <p><?php echo _("You will receive a reminder to <b>%s</b> one day before the event.",$this->email); ?></p>
+    <?php if($this->rvsp==0): ?>
+        <p><?php echo _("We will miss you! See you at the next event."); ?></p>
     <?php else: ?>
-    <p><?php echo _("We will miss you! See you at the next event."); ?></p>
+        <p><?php echo _("You will receive a reminder one day before the event."); ?></p>
     <?php endif; ?>
 </div>

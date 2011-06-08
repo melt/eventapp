6f6bfe6a39e04ac803e54a0cd0d7b26ae9547703
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-06-08 11:03:19 +0000

    Removed deprecated views
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4714 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- views/mail/moderate_user.php
+++
@@ -1,9 +0,0 @@
-<?php namespace nmvc; ?>
-
-<?php echo _("#### You are receiving this email since you have administrator privileges to %s ###",\nmvc\APP_NAME); ?>
-
-
-<?php echo _("%s with email %s has just logged into %s using Facebook and needs permissions to be able to proceed.",$this->name,$this->email,\nmvc\APP_NAME); ?>
-
-
-<?php echo _("Login to %s to grant the user permissions.",\APP_ROOT_URL); ?>

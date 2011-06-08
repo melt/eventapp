15af69fd5af850902239cadd6298054bc18927ef
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-06-08 09:27:42 +0000

    Updated email templates
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4704 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- views/mail/event_attendee_data.php
+++ views/mail/event_attendee_data.php
@@ -7,20 +7,25 @@
 — What's currently your most exciting project?
 <?php echo $this->mep; ?>
 
+
 --------------------
 
 — What's your biggest challenge right now?
 <?php echo $this->bc; ?>
+
 
 --------------------
 
 — How can we help you in general?
 <?php echo $this->gh; ?>
 
+
 --------------------
 
 — Schedule a Skype call?
 <?php echo $this->skype; ?>
 
+
+--------------------
 
 
--- views/mail/layout_plaintext.php
+++ views/mail/layout_plaintext.php
@@ -1,4 +1,3 @@
 <?php namespace nmvc; ?>
 <?php echo $this->content; ?>
-\n\n
-&copy; <?php echo date("Y"); ?> Sandbox Network. <?php if(isset($this->unsubscribe_link)): ?><a target="_blank" href="<?php echo $this->unsubscribe_link; ?>"><?php echo _("Click on this link"); ?></a> to unsubscribe from all future emails.<?php endif; ?>
+© <?php echo date("Y"); ?> Sandbox Network. <?php if(isset($this->unsubscribe_link)): ?><a target="_blank" href="<?php echo $this->unsubscribe_link; ?>"><?php echo _("Click on this link"); ?></a> to unsubscribe from all future emails.<?php endif; ?>

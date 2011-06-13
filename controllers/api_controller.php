2b55d4efa8c862f3bfeb62bbb5d48bfaf370b637
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-06-13 18:29:52 +0000

    Updated api function with past and present
    git-svn-id: http://svn.meltsoftware.com/eventapp/trunk@4751 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- controllers/api_controller.php
+++ controllers/api_controller.php
@@ -6,8 +6,21 @@ class ApiController extends userx\RestrictedController {
     /*
      * Public API call to get events in certain hub.
      */
-    public function get_events($hub_name) {
-        $results = \nmvc\EventModel::select()->where("hub->city")->isLike("%".$hub_name."%");
+    public function get_events($hub_name,$selection = "all") {
+        $selection = @$_GET["selection"];
+        switch($selection){
+            case "all":
+                $results = \nmvc\EventModel::select()->where("hub->city")->isLike("%".$hub_name."%")->orderBy("event_date","ASC");
+                break;
+            case "upcoming":
+                $results = \nmvc\EventModel::select()->where("hub->city")->isLike("%".$hub_name."%")->and("event_date")->isntLessThan(date('Y-m-d'))->orderBy("event_date","ASC");
+                break;
+            case "past":
+                $results = \nmvc\EventModel::select()->where("hub->city")->isLike("%".$hub_name."%")->and("event_date")->isLessThan(date('Y-m-d'))->orderBy("event_date","ASC");
+                break;
+        }
+        
+
         $response = array();
         foreach($results as $result):
            $response[] = array(

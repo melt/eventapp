65266aaf6e484f4734180a9c402aae6555bee765
Per Jonsson <per@recurringly.com>
2011-08-12 00:43:49 +0200

    Added API

---
+++ controllers/api_controller.php
@@ -0,0 +1,58 @@
+<?php namespace melt;
+
+class ApiController extends userx\RestrictedController {
+
+
+    /*
+     * Public API call to get events in certain hub.
+     */
+    public function get_events( $hub_name, $scope = "all" ) {
+        $scope = @$_GET["scope"];
+        switch($scope){
+            case "all":
+                $results = \melt\EventModel::select()->where("hub->city")->isLike("%".$hub_name."%")->orderBy("event_date","ASC");
+                break;
+            case "upcoming":
+                $results = \melt\EventModel::select()->where("hub->city")->isLike("%".$hub_name."%")->and("event_date")->isntLessThan(date('Y-m-d'))->orderBy("event_date","ASC");
+                break;
+            case "past":
+                $results = \melt\EventModel::select()->where("hub->city")->isLike("%".$hub_name."%")->and("event_date")->isLessThan(date('Y-m-d'))->orderBy("event_date","ASC");
+                break;
+        }
+        
+
+        $response = array();
+        foreach($results as $result):
+           $response[] = array(
+                "title"=>$result->view('title'),
+                "description"=>strip_tags($result->description),
+                "event_date"=>$result->view('event_date'),
+                "event_time"=>$result->view('event_time'),
+                "city"=>$result->view('city')
+                );
+        endforeach;
+
+        if( count($results) == 0 )
+            \melt\request\send_json_data( false );
+        else
+            \melt\request\send_json_data( $response );
+    }
+
+
+    public static function getDefaultPermission(userx\GroupModel $group = null) {
+        if ($group === null)
+            return "Allow";
+        else if ($group->context == userx\GroupModel::CONTEXT_GUEST)
+            return "Allow";
+        else if ($group->context == userx\GroupModel::CONTEXT_MEMBER)
+            return "Allow";
+        else if ($group->context == userx\GroupModel::CONTEXT_AMBASSADOR)
+            return "Allow";
+        else if ($group->context == userx\GroupModel::CONTEXT_ADMIN)
+            return "Allow";
+        else if ($group->context == userx\GroupModel::CONTEXT_SUPERADMIN)
+            return "Allow";
+        else
+            return false;
+    }
+}

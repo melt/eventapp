8232a3d25ed477ddf93ff719e1d053ad7e125f30
Per Jonsson <per@recurringly.com>
2011-08-30 12:44:26 +0200

    Added filter to people

--- views/people/index.php
+++ views/people/index.php
@@ -1,4 +1,10 @@
 <?php namespace melt; ?>
 <h2>People</h2>
 <p>This is a list of every person that has attended a Sandbox event.
-<?php echo data_tables\render_table("people", 'melt\userx\UserModel', array(),array("bPaginate"=>true,"bFilter"=>true,"bJQueryUI"=>false,"bInfo"=>true));
+<?php echo data_tables\render_table(
+        "people", 
+        'melt\userx\UserModel', 
+        array(),
+        array("bPaginate"=>true,"bSearch"=>true,"bFilter"=>true,"bJQueryUI"=>false,"bInfo"=>true),
+        expr("user_type")->isnt(0)
+        ); ?>
